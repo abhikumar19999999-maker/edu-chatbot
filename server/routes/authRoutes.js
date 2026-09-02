fe3e9face3a6224f083e992a9302886b2a706245
abhikumar19999999-maker <abhikumar19999999@gmail.com>
2026-09-02 18:03:38 +0530

    security: remove duplicate unprotected auth routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,21 +5,13 @@ import {
   loginUser
 } from "../controllers/authController.js";
 
-import {
-  validateBody
-} from "../middleware/validationMiddleware.js";
-
-import {
-  registerSchema,
-  loginSchema
-} from "../validators/authValidators.js";
-
-import {
-  authLimiter
-} from "../middleware/rateLimitMiddleware.js";
+import { validateBody } from "../middleware/validationMiddleware.js";
+import { registerSchema, loginSchema } from "../validators/authValidators.js";
+import { authLimiter } from "../middleware/rateLimitMiddleware.js";
 
 const router = express.Router();
 
+// Authentication endpoints are rate-limited and validated exactly once.
 router.post(
   "/register",
   authLimiter,
@@ -30,18 +22,6 @@ router.post(
 router.post(
   "/login",
   authLimiter,
-  validateBody(loginSchema),
-  loginUser
-);
-
-router.post(
-  "/register",
-  validateBody(registerSchema),
-  registerUser
-);
-
-router.post(
-  "/login",
   validateBody(loginSchema),
   loginUser
 );
