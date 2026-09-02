dee24225d23daf4ca22266140bb63c023d2e0174
abhikumar19999999-maker <abhikumar19999999@gmail.com>
2026-09-02 18:04:38 +0530

    security: rate limit feedback submissions

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -6,14 +6,9 @@ import {
 } from "../controllers/feedbackController.js";
 
 import authMiddleware from "../middleware/authMiddleware.js";
-
-import {
-  validateBody
-} from "../middleware/validationMiddleware.js";
-
-import {
-  feedbackSchema
-} from "../validators/feedbackValidators.js";
+import { validateBody } from "../middleware/validationMiddleware.js";
+import { feedbackSchema } from "../validators/feedbackValidators.js";
+import { feedbackLimiter } from "../middleware/rateLimitMiddleware.js";
 
 const router = express.Router();
 
@@ -21,13 +16,11 @@ router.use(authMiddleware);
 
 router.post(
   "/",
+  feedbackLimiter,
   validateBody(feedbackSchema),
   submitFeedback
 );
 
-router.get(
-  "/mine",
-  getMyFeedback
-);
+router.get("/mine", getMyFeedback);
 
 export default router;
